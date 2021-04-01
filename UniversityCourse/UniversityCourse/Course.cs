83e2de05b7fdec8a10d984cf41cf73c7e0ca8d2e
Saif Ali <mdalisaif4@gmail.com>
2021-04-01 20:49:56 +0600

    University Course Project

---
+++ UniversityCourse/UniversityCourse/Course.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityCourse
+{
+    class Course
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public Course() { }
+        public Course(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine("Course Name: " + name);
+            Console.WriteLine("ID: " + id);
+        }
+    }
+}
---
+++ UniversityCourse/UniversityCourse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Saif", "18-38710-3", 3.2f);
+            Student s2 = new Student("Mridul", "18-38296-3", 3.5f);
+            Department d1 = new Department("Computer Science", "CSE");
+            s1.Department = d1;
+            s2.Department = d1;
+            d1.AddStudent(s1);
+            d1.AddStudent(s2);
+            d1.AllStudents();
+        }
+    }
+}
