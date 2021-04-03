3bc14adab963905499d752f79156f9a2fd621947
Saif Ali <mdalisaif4@gmail.com>
2021-04-03 22:34:32 +0600

    Final Lab 2

---
+++ Test-Interface-2/Test-Interface-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Current("Saif", "2354", 3000);
+            acc1.showInfo();
+            acc1.Deposit(2000);            
+            acc1.showInfo();
+
+        }
+    }
+}
---
+++ Test-interface-1/Test-interface-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_interface_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            int sum = cal.sum(500, 600);
+            Console.WriteLine(sum);
+
+            Scientific sc = new Scientific();
+            int res = sc.XtoY(6, 3);
+            Console.WriteLine(res);
+
+        }
+    }
+}
